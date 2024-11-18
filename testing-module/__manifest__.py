f01f235bd826cbd1b87c35c1bf9647e99998eaa7
ezekielvictor <eazynoble3@gmail.com>
2024-11-18 18:04:25 +0100

    adding a test module

---
+++ testing-module/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "test-wesolved",
+    'version': '17.0.0.1.0',
+    'author': 'Ezekiel Victor',
+    'category': 'Custom',
+    'licence': 'AGPL-3',
+    'summary': 'Customization for testing',
+    'depends': ['hr'],
+    'data': [
+        'views/hr_employee.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
